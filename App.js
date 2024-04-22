204b9917eca6dd0f158a9585bcedc0d4a4a54c93
Daiara Dyba <daiaradyba@gmail.com>
2024-04-22 17:49:26 -0300

    Pasta Correta

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello, World!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // O componente View usará toda a tela
+    justifyContent: 'center', // Centraliza o conteúdo verticalmente
+    alignItems: 'center', // Centraliza o conteúdo horizontalmente
+    backgroundColor: '#fff', // Define a cor de fundo da View
   },
+  text: {
+    fontSize: 24, // Define o tamanho da fonte
+    color: 'black', // Define a cor do texto
+  }
 });
